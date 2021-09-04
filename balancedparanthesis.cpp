da08f1cacfadbd79dff214979eb4b350566ba401
varun chourasia <varunchourasiya45@gmail.com>
2021-09-04 23:35:41 +0530

    Add files via upload

---
+++ balancedparanthesis.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<stack>
+#include<algorithm>
+using namespace std;
+
+bool isvalid(string s){
+    int n = s.size();
+    stack<char> st;
+    bool ans=true;
+    for(int i=0;i<n;i++){
+        if(s[i]=='{' || s[i]=='[' ||s[i]=='('){
+            st.push(s[i]);
+        }else if(s[i]=='}'){
+            if(!st.empty() && st.top()=='{'){
+                st.pop();
+            }else{
+                ans=false;
+                break;
+            }
+        }
+        else if( s[i]==')'){
+            if(!st.empty() && st.top()=='('){
+                st.pop();
+            }else{
+                ans=false;
+                break;
+            }
+        }
+        else if (s[i]==']'){
+            if(!st.empty() && st.top()=='['){
+                st.pop();
+            }else{
+                ans=false;
+                break;
+            }
+        }
+    }
+    if(!st.empty())
+    return false;
+    else return ans;
+}
+
+int main(){
+    string s ="{[()]}";
+    if(isvalid(s)){
+        cout<<"string is valid"<<endl;
+    }else{
+        cout<<"string is invalid"<<endl;
+    }
+    return 0;
+}
